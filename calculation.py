c43d41b8d2d8488a3c0c27d73530cc073e98f91d
wabisachin <wabisachin1122@yahoo.co.jp>
2021-10-22 16:16:27 +0900

    各特徴量についてEV-hist２軸グラフをpng画像ファイルとしてそれぞれ保存する関数の完成(plot_a__biaxial_graph_for_ev)

--- calculation.py
+++ calculation.py
@@ -6,7 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import glob as gl
-from matplotlib import cm
+import re
+import module.module_calclation as mc
 
 #pandasのオプション設定
 pd.set_option("display.max_rows", None)
@@ -29,88 +30,126 @@ df = pd.read_csv(file_name)
 print(df.describe())
 
 #選択した特徴量を指定領域(bin)に分割して、各ビン内におけるpl_atr(ev)をグラフ化。(ビニング分析)
-def plot_ev_by_quartile(df, variable):#widthは区間幅, rightは境界値。例えばx2の場合は各区間幅(width)は1,境界値は7が妥当。
+# def plot_biaxial_graph_for_EV(df, variable):#widthは区間幅, rightは境界値。例えばx2の場合は各区間幅(width)は1,境界値は7が妥当。
     
-    ev_list = []
-    count_list = []
+#     ev_list = []
+#     count_list = []
+#     #特徴量別のbin区間定義。関数pd.cut(df,labels, bins, right=True) はデフォルトでritht=Trueなので,右辺の末端を含むことを考慮してbinの範囲を指定した。
+#     label_list ={
+#         "x1": {"bin_labels":["x1<=1", "1<x1<=2","2<x1<=3","3<x1<=4","4<x1<=5","5<x1<=6","6<x1<=7", "7<x1"], "bins":[0,1,2,3,4,5,6,7,100]},
+#         "x2": {"bin_labels":["x2<=1", "1<x2<=2","2<x2<=3","3<x2<=4","4<x2<=5","5<x2<=6","6<x2<=7", "7<x2"], "bins":[0,1,2,3,4,5,6,7,100]},
+#         "x3": {"bin_labels":["x3<=5", "5<x3<=10","10<x3<=15","15<x3<=20"], "bins":[-1,5,10,15,20]},
+#         "x4": {"bin_labels":["x4<=10", "10<x4<=20","20<x4<=30","30<x4<=40", "40<x4<=50", "50<x4<=60"], "bins":[-1,10,20,30,40,50,60]},
+#         "x5": {"bin_labels":["x5=0", "x5=1","x5=2","x5=3","x5=4","x5=5","5<x5=10","10<x5<=20","20<x5"], "bins":[-1,0,1,2,3,4,5,10,20,100]},
+#         "x6": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+#         "x7": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+#         "x8": {"bin_labels":["-10％以上", "-10％~-7.5％","-7.5％~-5.0％","-5.0％~-2.5％","-2.5％~0％", "0％~2.5％", "2.5％~5.0％","5.0％~7.5％","7.5％~10.0％","10.0％以上" ], "bins":[-1,-0.1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 0.1, 1]},
+#     }
+#     labels = label_list[variable]["bin_labels"]
+#     df["label"] = pd.cut(x=df[variable], bins=label_list[variable]["bins"], labels=label_list[variable]["bin_labels"])
+
+#     for label in labels:
+#         ev = df.loc[df["label"] == label]["pl_atr"].mean()
+#         count = df.loc[df["label"] == label]["label"].count()
+#         ev_list.append(ev)
+#         count_list.append(count)
+
+
+#     print(ev_list)
+#     print(count_list)
+
+#     #matplotlib.pypltによる描写
+#     fig, ax1 = plt.subplots()
+#     ax2 = ax1.twinx()
+#     ax1.grid(True)
+#     ax2.grid(False)
+#     color1, color2 = "skyblue", "orange"
+
+#     plt.title("Biaxial graph for {}(hist & EV)".format(variable))
+#     ax2.spines["left"].set_color(color1)
+#     ax2.spines["right"].set_color(color2)
+#     ax1.tick_params(axis='y', colors = color1)
+#     ax2.tick_params(axis='y', colors=color2)
+#     ax1.plot(labels, ev_list, color=color1, label="pl_atr")
+#     ax2.bar(labels, count_list, color=color2, alpha=0.6, width=0.5, label="count")
+#     plt.tick_params(labelsize=10)
+#     handler1, label1 = ax1.get_legend_handles_labels()
+#     handler2, label2 = ax2.get_legend_handles_labels()
+#     ax1.legend(handler1+handler2, label1+label2, loc="upper right")
+
+#     # plt.close(fig)
+#     plt.savefig("Biaxial graph for {}.png".format(variable))
+#     # plt.show()
+
+
+# print(df.head())
+# print(df.columns)
+# print(df.columns[1])
+# #特徴量一覧
+# variables = [col for col in df.columns if re.search(r'x\d', col)]
+# print(variables)
+
+def plot_all_biaxial_graph_for_EV(df):
+
     #特徴量別のbin区間定義。関数pd.cut(df,labels, bins, right=True) はデフォルトでritht=Trueなので,右辺の末端を含むことを考慮してbinの範囲を指定した。
-    label_list ={
-        "x1": {"bin_labels":["x1<=1", "1<x1<=2","2<x1<=3","3<x1<=4","4<x1<=5","5<x1<=6","6<x1<=7", "7<x1"], "bins":[0,1,2,3,4,5,6,7,100]},
-        "x2": {"bin_labels":["x2<=1", "1<x2<=2","2<x2<=3","3<x2<=4","4<x2<=5","5<x2<=6","6<x2<=7", "7<x2"], "bins":[0,1,2,3,4,5,6,7,100]},
-        "x3": {"bin_labels":["x3<=5", "5<x3<=10","10<x3<=15","15<x3<=20"], "bins":[-1,5,10,15,20]},
-        "x4": {"bin_labels":["x4<=10", "10<x4<=20","20<x4<=30","30<x4<=40", "40<x4<=50", "50<x4<=60"], "bins":[-1,10,20,30,40,50,60]},
-        "x5": {"bin_labels":["x5=0", "x5=1","x5=2","x5=3","x5=4","x5=5","5<x5=10","10<x5<=20","20<x5"], "bins":[-1,0,1,2,3,4,5,10,20,100]},
-        "x6": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
-        "x7": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
-        "x8": {"bin_labels":["-10％以上", "-10％~-7.5％","-7.5％~-5.0％","-5.0％~-2.5％","-2.5％~0％", "0％~2.5％", "2.5％~5.0％","5.0％~7.5％","7.5％~10.0％","10.0％以上" ], "bins":[-1,-0.1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 0.1, 1]},
-    }
-    labels = label_list[variable]["bin_labels"]
-    df["label"] = pd.cut(x=df[variable], bins=label_list[variable]["bins"], labels=label_list[variable]["bin_labels"])
+    # label_list ={
+    #     "x1": {"bin_labels":["x1<=1", "1<x1<=2","2<x1<=3","3<x1<=4","4<x1<=5","5<x1<=6","6<x1<=7", "7<x1"], "bins":[0,1,2,3,4,5,6,7,100]},
+    #     "x2": {"bin_labels":["x2<=1", "1<x2<=2","2<x2<=3","3<x2<=4","4<x2<=5","5<x2<=6","6<x2<=7", "7<x2"], "bins":[0,1,2,3,4,5,6,7,100]},
+    #     "x3": {"bin_labels":["x3<=5", "5<x3<=10","10<x3<=15","15<x3<=20"], "bins":[-1,5,10,15,20]},
+    #     "x4": {"bin_labels":["x4<=10", "10<x4<=20","20<x4<=30","30<x4<=40", "40<x4<=50", "50<x4<=60"], "bins":[-1,10,20,30,40,50,60]},
+    #     "x5": {"bin_labels":["x5=0", "x5=1","x5=2","x5=3","x5=4","x5=5","5<x5=10","10<x5<=20","20<x5"], "bins":[-1,0,1,2,3,4,5,10,20,100]},
+    #     "x6": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+    #     "x7": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+    #     "x8": {"bin_labels":["-10％以上", "-10％~-7.5％","-7.5％~-5.0％","-5.0％~-2.5％","-2.5％~0％", "0％~2.5％", "2.5％~5.0％","5.0％~7.5％","7.5％~10.0％","10.0％以上" ], "bins":[-1,-0.1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 0.1, 1]},
+    # }
+    #dfのcolumnから特徴量だけ抽出してリスト化
+    variables = [col for col in df.columns if re.search(r'x\d', col)]
 
-    for label in labels:
-        ev = df.loc[df["label"] == label]["pl_atr"].mean()
-        count = df.loc[df["label"] == label]["label"].count()
-        ev_list.append(ev)
-        count_list.append(count)
+    """x8がエラーを吐き出すので一時的に除外"""
+    variables.remove("x8")
 
+    """x8がエラーを吐き出すので一時的に除外"""
 
-    print(ev_list)
-    print(count_list)
+    length = len(variables)
+    #描写領域の確保
+    fig = plt.figure()
+    ax_list = []
 
-    fig, ax1 = plt.subplots()
-    ax2 = ax1.twinx()
-    ax1.grid(True)
-    ax2.grid(False)
-    color1 = cm.Set1.colors[1]
-    color2 =cm.Set1.colors[0]
-    plt.title("説明変数{}の各パラメータ値の出現頻度と期待値(pl_atr)".format(variable))
-    ax2.spines["left"].set_color(color1)
-    ax2.spines["right"].set_color(color2)
-    ax1.tick_params(axis='y', colors = color1)
-    ax2.tick_params(axis='y', colors=color2)
-    ax1.plot(labels, ev_list, color=color1, label="pl_atr")
-    ax2.bar(labels, count_list, color=color2, alpha=0.4, width=0.5, label="count")
-    # ax2.bar(labels, count_list, color=cm.Set1.colors[0])
-    # plt.tick_params(labelsize=10)
-    handler1, label1 = ax1.get_legend_handles_labels()
-    handler2, label2 = ax2.get_legend_handles_labels()
-    ax1.legend(handler1+handler2, label1+label2, loc="upper right")
+    for i, variable in enumerate(variables):
+        mc.plot_biaxial_graph_for_EV(df, variable)
+        # ev_list = []
+        # count_list = []
 
-    plt.show()
+        # labels = label_list[variable]["bin_labels"]
+        # df["label"] = pd.cut(x=df[variable], bins=label_list[variable]["bins"], labels=label_list[variable]["bin_labels"])
 
-        
-print(df.head())
-plot_ev_by_quartile(df, "x2")
+        # for label in labels:
+        #     ev = df.loc[df["label"] == label]["pl_atr"].mean()
+        #     count = df.loc[df["label"] == label]["label"].count()
+        #     ev_list.append(ev)
+        #     count_list.append(count)
 
-# sns.countplot(x="x4", data = df)
-# sns.countplot(x="x2", data = df)
-# sns.displot(df["x2"], kde = False, bins = 10)
-# plt.show()
+        # print(ev_list)
+        # print(count_list)
 
-# df.plot.scatter(x="x3", y="x5")
-# plt.show()
+        # ax1 = fig.add_subplot((length/2)+1, 2, i+1)
+        # ax2 = ax1.twinx()
+        # color1, color2 = "skyblue", "orange"
+        # plt.title("Biaxial graph for {}(hist & EV)".format(variable))
+        # ax2.spines["left"].set_color(color1)
+        # ax2.spines["right"].set_color(color2)
+        # ax1.tick_params(axis='y', colors = color1)
+        # ax2.tick_params(axis='y', colors=color2)
+        # ax1.plot(labels, ev_list, color=color1, label="pl_atr")
+        # ax2.bar(labels, count_list, color=color2, alpha=0.6, width=0.5, label="count")
+        # plt.tick_params(labelsize=10)
+        # handler1, label1 = ax1.get_legend_handles_labels()
+        # handler2, label2 = ax2.get_legend_handles_labels()
+        # ax1.legend(handler1+handler2, label1+label2, loc="upper right")
+    
+    # plt.show()
 
-
-# print(df.dtypes)
-# scatter = df.plot.scatter(x="x1", y="pl_atr")
-# scatter.plot()
-# plt.show()
-# sns.scatterplot(x="x1", y="pl_atr", data=df, color="red", alpha=0.5)
-# print(df.corr())
-# print(df.describe())
-# plt.scatter(df["x1"], df["x2"])
-# plt.xlabel("x2")
-# plt.ylabel("pl_atr")
-# plt.show()
-
-
-#2<x2, position="l", x7=0のヒートマップをx2,x4の二次元で作成。目的変数y:pl_atr
-# df_x7_0 = df.loc[df["x7"]==0]
-
-
-
-
-
+plot_all_biaxial_graph_for_EV(df)
 
 
 
@@ -150,3 +189,43 @@ plot_ev_by_quartile(df, "x2")
 # print(df.loc[(df["x2"]>2) &(df["x1"]<2) & (df["x1"]>1) & (df["position"]=="l") &(df["x4"] <30)].describe())
 # print("-----x2>2, 1<x1<2,かつl, かつx4>=30の結果-----")
 # print(df.loc[(df["x2"]>2) &(df["x1"]<2) & (df["x1"]>1) & (df["position"]=="l") &(df["x4"] >= 30)].describe())
+
+# print("-----lの結果-----")
+# print(df.loc[df["position"]=="l"].describe())
+# print("-----l, かつ0<x2<1の結果-----")
+# print(df.loc[(df["x2"]>0) &(df["x2"]<1) & (df["position"]=="l")].describe())
+# print("-----l, かつ1<x2<2の結果-----")
+# print(df.loc[(df["x2"]>1) &(df["x2"]<2) & (df["position"]=="l")].describe())
+# print("-----l, かつ2<x2<3の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")].describe())
+# print("-----l, かつ3<x2<4の結果-----")
+# print(df.loc[(df["x2"]>3) &(df["x2"]<4) & (df["position"]=="l")].describe())
+# print("-----l, かつ4<x2<5の結果-----")
+# print(df.loc[(df["x2"]>4) &(df["x2"]<5) & (df["position"]=="l")].describe())
+# print("-----l, かつ5<x2<6の結果-----")
+# print(df.loc[(df["x2"]>5) &(df["x2"]<6) & (df["position"]=="l")].describe())
+# print("-----l, かつ6<x2<7の結果-----")
+# print(df.loc[(df["x2"]>6) &(df["x2"]<7) & (df["position"]=="l")].describe())
+# print("-----l, かつ7<x2<8の結果-----")
+# print(df.loc[(df["x2"]>7) &(df["x2"]<8) & (df["position"]=="l")].describe())
+# print("-----l, かつ8<x2<9の結果-----")
+# print(df.loc[(df["x2"]>8) &(df["x2"]<9) & (df["position"]=="l")].describe())
+# print("-----l, かつ9<x2の結果-----")
+# print(df.loc[(df["x2"]>9)& (df["position"]=="l")].describe())
+# print("---------------")
+# print("-----l, かつ2<x2<3の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")].describe())
+# print("-----l, かつ2<x2<3,かつx7=0の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")&(df["x7"]==0)].describe())
+# print("-----l, かつ2<x2<3,かつx7=1の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")&(df["x7"]==1)].describe())
+# print("-----l, かつ2<x2<3,かつ1<x1<2の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")&(df["x1"]>1)&(df["x1"]<2)].describe())
+# print("-----l, かつ2<x2<3,かつ2<x1<3の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")&(df["x1"]>2)&(df["x1"]<3)].describe())
+# print("-----l, かつ2<x2<3,かつ3<x1<4の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")&(df["x1"]>3)&(df["x1"]<4)].describe())
+# print("-----l, かつ2<x2<3,かつ4<x1<5の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")&(df["x1"]>4)&(df["x1"]<5)].describe())
+# print("-----l, かつ2<x2<3,かつ5<x1の結果-----")
+# print(df.loc[(df["x2"]>2) &(df["x2"]<3) & (df["position"]=="l")&(df["x1"]>5)].describe())
--- module/module_calclation.py
+++ module/module_calclation.py
@@ -48,6 +48,7 @@ total_P(利益の合計額), total_L(損失の合計額), max_P（最大利益pt
 
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #時系列データ(.csv)から[日付、始値、高値、安値、終値]を抽出し、Dataflame型に変換する関数
 # def get_data(fileName):
@@ -180,11 +181,76 @@ def calc_EV(trade_result, display=1):
 
     return results
 
+#選択した特徴量を指定領域(bin)毎に分割して、各ビン内におけるpl_atr(ev)をヒストグラムと共に２軸グラフ化。(ビニング分析)
+def plot_biaxial_graph_for_EV(df, variable, save=1):#widthは区間幅, rightは境界値。例えばx2の場合は各区間幅(width)は1,境界値は7が妥当。
+    
+    ev_list = []
+    count_list = []
+    #特徴量別のbin区間定義。関数pd.cut(df,labels, bins, right=True) はデフォルトでritht=Trueなので,右辺の末端を含むことを考慮してbinの範囲を指定した。
+    label_list ={
+        "x1": {"bin_labels":["0~1", "1~2","2~3","3~4","4~5","5~6","6~7", "7~"], "bins":[0,1,2,3,4,5,6,7,100]},
+        "x2": {"bin_labels":["~1", "1~2","2~3","3~4","4~5","5~6","6~7", "7~"], "bins":[0,1,2,3,4,5,6,7,100]},
+        "x3": {"bin_labels":["~5", "5~10","10~15","15~20"], "bins":[-1,5,10,15,20]},
+        "x4": {"bin_labels":["~10", "10~20","20~30","30~40", "40~50", "50~60"], "bins":[-1,10,20,30,40,50,60]},
+        "x5": {"bin_labels":["0", "1","2","3","4","5","5~10","10~20","20~"], "bins":[-1,0,1,2,3,4,5,10,20,100]},
+        "x6": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+        "x7": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+        "x8": {"bin_labels":["-10％以上", "-10％~-7.5％","-7.5％~-5.0％","-5.0％~-2.5％","-2.5％~0％", "0％~2.5％", "2.5％~5.0％","5.0％~7.5％","7.5％~10.0％","10.0％以上" ], "bins":[-1,-0.1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 0.1, 1]},
+    }
+    # label_list ={
+    #     "x1": {"bin_labels":["x1<=1", "1<x1<=2","2<x1<=3","3<x1<=4","4<x1<=5","5<x1<=6","6<x1<=7", "7<x1"], "bins":[0,1,2,3,4,5,6,7,100]},
+    #     "x2": {"bin_labels":["x2<=1", "1<x2<=2","2<x2<=3","3<x2<=4","4<x2<=5","5<x2<=6","6<x2<=7", "7<x2"], "bins":[0,1,2,3,4,5,6,7,100]},
+    #     "x3": {"bin_labels":["x3<=5", "5<x3<=10","10<x3<=15","15<x3<=20"], "bins":[-1,5,10,15,20]},
+    #     "x4": {"bin_labels":["x4<=10", "10<x4<=20","20<x4<=30","30<x4<=40", "40<x4<=50", "50<x4<=60"], "bins":[-1,10,20,30,40,50,60]},
+    #     "x5": {"bin_labels":["x5=0", "x5=1","x5=2","x5=3","x5=4","x5=5","5<x5=10","10<x5<=20","20<x5"], "bins":[-1,0,1,2,3,4,5,10,20,100]},
+    #     "x6": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+    #     "x7": {"bin_labels":["0", "1"], "bins":[-1,0,1]},
+    #     "x8": {"bin_labels":["-10％以上", "-10％~-7.5％","-7.5％~-5.0％","-5.0％~-2.5％","-2.5％~0％", "0％~2.5％", "2.5％~5.0％","5.0％~7.5％","7.5％~10.0％","10.0％以上" ], "bins":[-1,-0.1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 0.1, 1]},
+    # }
+    labels = label_list[variable]["bin_labels"]
+    df["label"] = pd.cut(x=df[variable], bins=label_list[variable]["bins"], labels=label_list[variable]["bin_labels"])
+
+    for label in labels:
+        ev = df.loc[df["label"] == label]["pl_atr"].mean()
+        count = df.loc[df["label"] == label]["label"].count()
+        ev_list.append(ev)
+        count_list.append(count)
+
+
+    print(ev_list)
+    print(count_list)
+
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.grid(True)
+    ax2.grid(False)
+    color1, color2 = "skyblue", "orange"
+
+    # plt.title("説明変数{}の各パラメータ値の出現頻度と期待値(pl_atr)".format(variable))
+    plt.title("Biaxial graph for {}(hist & EV)".format(variable))
+    ax1.set_xlabel(variable)
+    ax2.spines["left"].set_color(color1)
+    ax2.spines["right"].set_color(color2)
+    ax1.tick_params(axis='y', colors = color1)
+    ax2.tick_params(axis='y', colors=color2)
+    ax1.plot(labels, ev_list, color=color1, label="pl_atr")
+    ax2.bar(labels, count_list, color=color2, alpha=0.6, width=0.5, label="count")
+    # ax2.bar(labels, count_list, color=cm.Set1.colors[0])
+    plt.tick_params(labelsize=10)
+    handler1, label1 = ax1.get_legend_handles_labels()
+    handler2, label2 = ax2.get_legend_handles_labels()
+    ax1.legend(handler1+handler2, label1+label2, loc="upper right")
+
+    #save=0なら画像保存せずにfigureに表示する
+    plt.savefig("Biaxial graph for {}.png".format(variable)) if save==1 else plt.show()
 
 
 
 
 
+
+
+#------------------------------------<現在は使ってない関数>----------------------------------------------
 
 #トレード結果の集計。３つの売買区分（合算、ロング、ショート）別に集計して、結果を返す。
 def summary_PL_old(trade_list, display=1):
